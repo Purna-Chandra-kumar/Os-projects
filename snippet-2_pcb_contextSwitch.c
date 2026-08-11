4929519820ce4b8b021bf85be154f6c3dafeb8eb
Purna Chandra Kumar Panigrahi <santoshpanigrahi75057@gmail.com>
2026-08-11 17:53:59 +0530

    Refactor contextSwitch function and improve comments
    Updated contextSwitch function and main function comments.

--- snippet-2_pcb_contextSwitch.c
+++ snippet-2_pcb_contextSwitch.c
@@ -8,10 +8,10 @@ struct PCB{
     char state[10];
 };
 
-void contextSwitch(struct PCB *current, struct PCB *next){
+void contextSwitch(struct PCB *current, struct PCB *next)
+{
     printf("Saving State of Process : %d\n", current->pid);
     strcpy(current->state, "READY");
-
     printf("Loading State of Process : %d\n", next->pid);
     strcpy(next->state, "RUNNING");
     printf("Context Switch: P%d -> P%d\n\n", current->pid, next->pid);
@@ -21,10 +21,9 @@ void contextSwitch(struct PCB *current, struct PCB *next){
 int main(){
     struct PCB p1 = {1, 120, 45, "RUNNING"};
     struct PCB p2 = {2, 45, 10, "READY"};
-
     printf("Initially Processe are Running: %d %d", p1.pid, p2.pid);
 
-    //! Making the Context Switch
+    //! Making the Context Switch Imp step
     contextSwitch(&p1, &p2);
 
     printf("Process %d State : %s\n", p1.pid, p1.state);
